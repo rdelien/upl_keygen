b909aebc8c74ffa54f9371bea06f1ac88066a123
Robert Deliën <81567317+rdelien@users.noreply.github.com>
2020-07-27 17:31:54 +0200

    Updated option 9

--- main.c
+++ main.c
@@ -17,16 +17,16 @@ static struct option_t {
 	char  *name;
 	char  *description;
 } option[NR_OF_OPTIONS] = {
-	{ "B4",      "Remote Control" },
-	{ "B21",     "DA Prot" },
-	{ "B22",     "DA Jitt" },
-	{ "B10",     "Sequence Control" },
-	{ "B6",      "Coher" },
-	{ "B33",     "ITU 033" },
-	{ "B8",      "Phone Test" },
-	{ "B23",     "Coded" },
-	{ "B9",      "3G Tests" },
-	{ "Unknown", "Is accepted, but doesn’t enable an option" }
+	{ "B4",  "Remote Control" },
+	{ "B21", "DA Prot" },
+	{ "B22", "DA Jitt" },
+	{ "B10", "Sequence Control" },
+	{ "B6",  "Coher" },
+	{ "B33", "ITU 033" },
+	{ "B8",  "Phone Test" },
+	{ "B23", "Coded" },
+	{ "B9",  "3G Tests" },
+	{ "U81", "DAI tests REL99 with CMU" }
 };
 
 
